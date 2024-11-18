e17a670aafcc4c1b14cb469d929458207fbae479
Gusta-PA <gustavo.almeida@sptech.school>
2024-11-18 19:32:49 -0300

    Cadastro Funcionando

--- Banco de Dados/Banco de Dados.sql
+++ Banco de Dados/Banco de Dados.sql
@@ -6,10 +6,14 @@ idUser int primary key auto_increment,
 nome varchar(45),
 email varchar(45),
 dtNasc date,
-senha varchar(50)
+senha varchar(300)
 );
 
 INSERT INTO usuarios(nome) VALUES
-('gustavo');
+ ('gustavo');
+
+INSERT INTO usuarios (nome, email, dtNasc, senha) VALUES
+('Gustavo', 'gustavo@gmail.com', '2005-09-28', sha2('gustvo', 256));
 
 SELECT * FROM usuarios;
+truncate table usuarios;
--- Site/src/models/usuarioModel.js
+++ Site/src/models/usuarioModel.js
@@ -3,7 +3,8 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idUser, nome, email, dtNasc, senha as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT idUser, nome, email, dtNasc, senha FROM usuarios
+             WHERE email = '${email}' AND senha = sha2('${senha}' , 256);
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -17,7 +18,7 @@ function cadastrar(nome, email, dtNasc, senha ) {
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
         INSERT INTO usuarios (nome, email, dtNasc, senha) VALUES 
-        ('${nome}', '${email}', '${dtNasc}', '${senha}');
+        ('${nome}', '${email}', '${dtNasc}', sha2('${senha}', 256));
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
